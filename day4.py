331b9790b2d9e5377aa631932ba953f540a9fe23
myoung72 <myoung72@jhu.edu>
2023-01-18 12:59:41 -0500

    day 4 complete

---
+++ day4.py
@@ -0,0 +1,25 @@
+import re  # here we go with regex.
+
+total_pairs = 0  # ones where one fully contains the other
+
+overlaps = 0  # i.e. they intersect at all
+
+f = open("day4.txt", "r")
+for line in f.readlines():
+    digits = [int(x) for x in re.findall(r"\d+", line)]
+    print(digits)
+    # should always be 4 numbers...at least, for part 1
+    first_range = set(range(digits[0], digits[1] + 1))
+    second_range = set(range(digits[2], digits[3] + 1))
+
+    intersect = first_range.intersection(second_range)
+    if intersect == first_range or intersect == second_range:
+        # print(intersect, first_range, second_range)
+        total_pairs += 1
+        overlaps += 1
+    elif len(intersect) != 0:
+        # print(intersect)
+        overlaps += 1
+print(total_pairs)
+print(overlaps)
+f.close()
